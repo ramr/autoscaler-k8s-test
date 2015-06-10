d2679607bb39b70a41e41dfd84cc0956c087c3dc
ramr <smitram@gmail.com>
2015-06-10 13:52:33 -0700

    Fix typo + make test script executable.

--- testautoscaler.sh
+++ testautoscaler.sh
@@ -3,5 +3,5 @@
 K8S_SOURCE_DIR=~/work/kubernetes
 
 make
-${K8S/SOURCE_DIR}/cluster/kubectl.sh create -f  testas.json 
-${K8S/SOURCE_DIR}/cluster/kubectl.sh create -f  testrc.json 
+${K8S_SOURCE_DIR}/cluster/kubectl.sh create -f  testas.json 
+${K8S_SOURCE_DIR}/cluster/kubectl.sh create -f  testrc.json 
